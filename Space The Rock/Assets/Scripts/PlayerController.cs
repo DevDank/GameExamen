b2fe4fd643a2cd98c37607bcd182c8c35136c71a
daniel <dkempers@roc-dev.com>
2021-02-11 15:35:27 +0100

    correct project folder
    i have replaced the project folder with the right version of the game

---
+++ Space The Rock/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float verticalInput;
+    private float movementSpeed = 500;
+    private float turnSpeed = 100;
+    public GameObject laser;
+    public GameObject firePoint;
+    public GameObject firePoint2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        //fly code
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed * verticalInput);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(laser, firePoint.transform.position, firePoint.transform.rotation);
+            Instantiate(laser, firePoint2.transform.position, firePoint2.transform.rotation);
+        }
+    }
+}
