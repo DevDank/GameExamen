6160adf99345857f7dbebac995a11846d0f9e2d2
daaaaniel <danieluitalmere@gmail.com>
2021-03-23 10:21:45 +0100

    comment
    added detailed comments to the entire code

--- Space The Rock/Assets/Scripts/Astroid.cs
+++ Space The Rock/Assets/Scripts/Astroid.cs
@@ -7,7 +7,8 @@ public class Astroid : MonoBehaviour {
     // Use this for initialization
     private Rigidbody rb;
     private float spinSpeed = 10f;
-    
+    public GameObject mediumAstroid;
+    public GameObject smallAstroid;
     void Start()
     {
         
@@ -16,18 +17,46 @@ public class Astroid : MonoBehaviour {
 
     void Update()
     {
+        //spin in random direction
         transform.Rotate(new Vector3((Random.Range(1, 360)), (Random.Range(1, 360)), (Random.Range(1, 360))) * Time.deltaTime);
+    
     }
     
 
-// kill space ship on impact
+    // kill space ship on impact
     private void OnCollisionEnter(Collision other) {
         if(other.gameObject.tag == "Laser"){
             Destroy(gameObject);
-        } else Destroy(other.gameObject);
+            // if laser hit big rock destroy and start BigAstroid
+            if(gameObject.tag == "Big"){
+                Destroy(other.gameObject);
+                BigAstroid();
+                Destroy(gameObject);
+               
+            // if laser hit medium rock destroy and start MedAstroid
+            } else if(gameObject.tag == "Med"){
+                Destroy(other.gameObject);
+                MedAstroid();
+                Destroy(gameObject);
+                
+            } else Destroy(gameObject);
 
-
-        
+        } else Destroy(other.gameObject);   
+    
     }
-   
+
+    // turn big astroid into 2 medium
+    private void BigAstroid(){
+        Debug.Log("xDGROOT");
+        Instantiate(mediumAstroid, gameObject.transform.position, gameObject.transform.rotation);
+        Instantiate(mediumAstroid, gameObject.transform.position, gameObject.transform.rotation);
+    }
+
+    // turn medium astroid into 2 small
+    private void MedAstroid(){
+        Debug.Log("MEDIUM BRUH");
+        Instantiate(smallAstroid, gameObject.transform.position, gameObject.transform.rotation);
+        Instantiate(smallAstroid, gameObject.transform.position, gameObject.transform.rotation);
+    }
+
 }
--- Space The Rock/Assets/Scripts/PlayerController.cs
+++ Space The Rock/Assets/Scripts/PlayerController.cs
@@ -13,6 +13,8 @@ public class PlayerController : MonoBehaviour
     public GameObject firePoint2;
     private bool onCooldown = false;
     private float cooldownTime = 0.2f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,21 +24,28 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //get arrowkeys or wasd input
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
-        //fly code
+        //make ship rotate
         transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+
+        //make ship move forward andbackwards
         transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed * verticalInput);
 
+        //get space bar input
     if (Input.GetButton("Jump") && onCooldown == false)
         {
+            // spawn laser at firepoint location and rotation
             Instantiate(laser, firePoint.transform.position, firePoint.transform.rotation);
-            Instantiate(laser, firePoint2.transform.position, firePoint2.transform.rotation);
+            
+            // start firerate cooldown
             StartCoroutine(FireCooldown());
         }
     }
 
+    // firerate cooldown
     IEnumerator FireCooldown()
     {
         onCooldown = true;
