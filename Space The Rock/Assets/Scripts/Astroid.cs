5f80ad741b1a50a768e875bdd86ac4e4da458058
daaaaniel <danieluitalmere@gmail.com>
2021-03-24 12:23:50 +0100

    rock explode / added gamemanager
    rocks explode on laser impact and added gamemanager script for spawner

--- Space The Rock/Assets/Scripts/Astroid.cs
+++ Space The Rock/Assets/Scripts/Astroid.cs
@@ -7,11 +7,12 @@ public class Astroid : MonoBehaviour {
     // Use this for initialization
     private Rigidbody rb;
     private float spinSpeed = 10f;
+    private float maxSpeed = 100f;
     public GameObject mediumAstroid;
     public GameObject smallAstroid;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
 
     }
 
@@ -26,26 +27,23 @@ public class Astroid : MonoBehaviour {
     // kill space ship on impact
     private void OnCollisionEnter(Collision other) {
         if(other.gameObject.tag == "Laser"){
-            Destroy(gameObject);
+            
             // if laser hit big rock destroy and start BigAstroid
             if(gameObject.tag == "Big"){
-                Destroy(other.gameObject);
-                BigAstroid();
-                Destroy(gameObject);
-               
+                BigAstroid();              
             // if laser hit medium rock destroy and start MedAstroid
             } else if(gameObject.tag == "Med"){
-                Destroy(other.gameObject);
-                MedAstroid();
-                Destroy(gameObject);
-                
-            } else Destroy(gameObject);
-
-        } else Destroy(other.gameObject);   
-    
+                MedAstroid();             
+            }
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        } else if(other.gameObject.tag == "player"){
+            Destroy(gameObject);
+        }
+ // astroid destroyed zichzelf met collision omdat spawned op zelfde positie   
     }
 
-    // turn big astroid into 2 medium
+   
     private void BigAstroid(){
         Debug.Log("xDGROOT");
         Instantiate(mediumAstroid, gameObject.transform.position, gameObject.transform.rotation);
@@ -57,6 +55,13 @@ public class Astroid : MonoBehaviour {
         Debug.Log("MEDIUM BRUH");
         Instantiate(smallAstroid, gameObject.transform.position, gameObject.transform.rotation);
         Instantiate(smallAstroid, gameObject.transform.position, gameObject.transform.rotation);
+    }
+
+
+    void FixedUpdate() {
+        if(rb.velocity.magnitude > maxSpeed){
+            rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+        }
     }
 
 }
